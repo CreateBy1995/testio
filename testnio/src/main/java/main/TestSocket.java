c78557dd0bec599990a54a92a76b8e10df28d2f1
sunshine <sunshine>
2019-07-22 14:53:56 +0800

    io

--- testnio/src/main/java/main/TestSocket.java
+++ testnio/src/main/java/main/TestSocket.java
@@ -50,29 +50,6 @@ public class TestSocket {
         fileChannel.close();
     }
     @Test
-    public void clientNIO() throws IOException {
-        // 开启一个客户端通道
-        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1",90)) ;
-        // 文件路径
-        File file = new File("");
-        String filePath = file.getCanonicalPath();
-        // 以读的模式开启一个文件通道，将此文件发送到服务端
-        FileChannel fileChannel = FileChannel.open(Paths.get(filePath+"//testChannel.txt"), StandardOpenOption.READ) ;
-        // 创建一个存放48个字节的缓冲区
-        ByteBuffer byteBuffer = ByteBuffer.allocate(48) ;
-        while (fileChannel.read(byteBuffer)!=-1){
-            // 将缓冲区切换成读取数据的模式
-            byteBuffer.flip() ;
-            // 将缓冲区的数据发送给服务端
-            socketChannel.write(byteBuffer) ;
-            // 重置缓冲区状态
-            byteBuffer.clear() ;
-        }
-        // 关闭资源
-        socketChannel.close();
-        fileChannel.close();
-    }
-    @Test
     public void blockingServer() throws IOException {
         // 开启一个服务端通道
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open() ;
@@ -146,6 +123,10 @@ public class TestSocket {
         // 轮询获取选择器上的 准备就绪 的事件
         // select 返回的是准备就绪的 SelectionKey的数量 例如有两个客户端可读 就返回2
         // selector有两个属性 一个叫publicKeys存放所有注册到选择器上的key 一个叫publicSelectedKeys 存放所有准备完毕的key
+
+        // select 是水平触发的(一直触发) 应用程序如果没有完成对一个已经就绪的文件描述符进行IO操作，那么之后每次select调用还是会将这些文件描述符通知进程
+        // 也就是说如果 我检测到一个socket有可读事件  但是并没有对他进行IO处理 那么在下次select中还是会一直返回
+        // 对应水平触发的就是边沿触发 也就是只触发一次 就是说不管你有没有IO处理 我只返回一次  也就是epoll中 ET(边沿触发)和LT(水平触发)
         while (selector.select() > 0){
             // 获取所有准备就绪的事件
             // selector.selectedKeys()方法返回的是 publicSelectedKeys  表示所有已经准备就绪的的
@@ -164,7 +145,6 @@ public class TestSocket {
                     socketChannel.register(selector,keys) ;
                     // 如果准备好的事件 是可读事件
                 }else if(selectionKey.isReadable()){
-                    System.out.println("ssss");
                     // 获取通道并打印读取到的数据
                     SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
                     ByteBuffer byteBuffer = ByteBuffer.allocate(1024) ;
@@ -182,6 +162,7 @@ public class TestSocket {
                     // socketChannel.read(buffer) 是有返回值的，这种情况下返回值是 -1，所以如果 read 方法返回的是 -1，就可以关闭和这个客户端的连接了。
                     // 如果不关闭 则selector.select()每次都会检测到这个关闭的客户端的 读事件 仍然可用  但是socketChannel.read(byteBuffer)始终为-1
                     // 导致服务器一直进行轮询
+                    // 这点与在linux下的epoll空轮询bug有点类似  最终都会导致CPU占用率上升
                     if (i == -1){
                         socketChannel.close();
                     }
